7d8d74755c7b8b916084a2f3b0165461264390d0
UbuntuKylin <danaekola@outlook.com>
2015-07-27 03:06:03 +1200

    Mainly modify  Cart.js

--- pos_v2/main/model/time.js
+++ pos_v2/main/model/time.js
@@ -3,15 +3,15 @@ function Time( year,month,date,hour,minute,second ) {
   this.month = month;
   this.date = date;
   this.hour = hour;
-  this.minute=minute;
-  this.second=second;
+  this.minute = minute;
+  this.second = second;
 }
-Time.prototype.timer=function(){
-
+Time.prototype.timer = function(){
   var dateDigitToString = function(num){
-    return num < 10 ? '0' + num : num;
+	  return num < 10 ? '0' + num : num;
   };
-  var date=new Date();
-  var time=new Time(date.getFullYear(),dateDigitToString(date.getMonth()+1),dateDigitToString(date.getDate()),dateDigitToString(date.getHours()),dateDigitToString(date.getMinutes()),dateDigitToString(date.getSeconds()));
-  return time.year + '年' + time.month + '月' + time.date + '日 ' + time.hour + ':' + time.minute + ':' + time.second;
+
+  var date = new Date();
+  var time = new Time(date.getFullYear(),dateDigitToString(date.getMonth()+1),dateDigitToString(date.getDate()),dateDigitToString(date.getHours()),dateDigitToString(date.getMinutes()),dateDigitToString(date.getSeconds()));
+    return time.year + '年' + time.month + '月' + time.date + '日 ' + time.hour + ':' + time.minute + ':' + time.second;
 };
